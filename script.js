c30d1fe88dfcb1fa5937bd3539959e01f1b8255b
ArjunSingh1322 <as3388776@gmail.com>
2025-02-13 12:38:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+let first = document.querySelector("#firstname");
+let last = document.querySelector("#lastname");
+let btn = document.querySelector("#button"); // Fixed missing quotes around #button
 
+btn.addEventListener("click", getFormValue);
+
+function getFormValue() {
+    let str2 = first.value + " " + last.value; // Added space between first and last name
+    alert(str2); // Shows an alert with the combined names
 }
